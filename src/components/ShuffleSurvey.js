fd9a84dc66ccfd11f2f018384d28c66844d4dd7e
ae0n1an <67774277+ae0n1an@users.noreply.github.com>
2021-07-15 13:27:01 +1000

    started designing card

--- src/components/ShuffleSurvey.js
+++ src/components/ShuffleSurvey.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { FBaseDB } from '../tools/Firebase'
 import { Link } from 'react-router-dom'
 import TopNav from './Topnav'
+import SurveyCard from './SurveyCard'
 import { useAuthContext } from '../context/AuthProviders'
 
 const ShuffleSurvey = () => {
     return (
         <div>
             <TopNav />
+            <div className="cardDiv">
+                <SurveyCard />
+            </div>
         </div>
     )
 }
--- src/components/SurveyCard.js
+++ src/components/SurveyCard.js
@@ -2,13 +2,27 @@ import React from 'react'
 
 const SurveyCard = ({ surveyInfo }) => {
     return (
+        <div className="surveyBox">
+                <span className="cardTitle">Title</span>
+                <span className="cardDesc">this is a description for a survey card</span>
+                <span className="cardCredit">Credit: 100</span>
+                <span className="cardTime">20 min</span>
+                <span className="cardPeopleText">200</span>
+                <img src="images/tags.png" className="cardTags"/>
+                <img src="images/likes.png" className="cardLikes"/>
+                <img src="images/people.png" className="cardPeople"/>
+                <img src="images/clock.png" className="cardClock"/>
+        </div>
+        /*
         <div style={styles}>
             <p>{surveyInfo.title}</p>
-            <p>{surveyInfo.description}</p>
-            <p>{surveyInfo.credit}</p>
-            <p>{surveyInfo.duration}</p>
-            <p>{surveyInfo.peopleCap}</p>
+            <p>{surveyInfo.desc}</p>
+            <p>{surveyInfo.creditBoost}</p>
+            <p>{surveyInfo.length}</p>
+            <p>{surveyInfo.link}</p>
+            <p>{surveyInfo.publisherID}</p>
         </div>
+        */
     )
 }
 
